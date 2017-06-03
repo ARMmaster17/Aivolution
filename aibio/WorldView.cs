5af3e08a845a616fc82acd7010e423bf40897aae
ARMmaster17 <wordman05@gmail.com>
2017-06-03 18:28:55 -0400

    Added WorldView WF control.

---
+++ aibio/WorldView.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aibio
+{
+    public partial class WorldView : UserControl
+    {
+        private bool _viewDragMode = false;
+        private Bitmap _fullWorldView;
+        private Point _viewBaseCoord;
+        private Point _lastMousePosition;
+
+        public WorldView()
+        {
+            InitializeComponent();
+            _fullWorldView = new Bitmap(100, 100);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _lastMousePosition = MousePosition;
+            _viewDragMode = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Only continue if the mouse is currently pressed down over the viewport.
+            if (!_viewDragMode) return;
+            // Because this is an event, we capture the current value of MousePosition
+            // in case it changes while we do the calculations.
+            Point currentMousePosition = MousePosition;
+            // Calculate delta by finding the difference between the two points.
+            Point deltaMousePosition = _lastMousePosition - (Size) currentMousePosition;
+            // Use delta value to modify viewport center relative to the world.
+            _viewBaseCoord += (Size) deltaMousePosition;
+            // Cycle _lastMousePosition for the next movement calculation.
+            _lastMousePosition = currentMousePosition;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _viewDragMode = false;
+        }
+
+        private void WorldView_Load(object sender, EventArgs e)
+        {
+            this.Dock = DockStyle.Fill;
+        }
+    }
+}
--- aivolution/Form1.Designer.cs
+++ aivolution/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.worldView1 = new aibio.WorldView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(829, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.worldView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(829, 537);
+            this.splitContainer1.SplitterDistance = 592;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // worldView1
+            // 
+            this.worldView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.worldView1.Location = new System.Drawing.Point(0, 0);
+            this.worldView1.Name = "worldView1";
+            this.worldView1.Size = new System.Drawing.Size(592, 537);
+            this.worldView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 561);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private aibio.WorldView worldView1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
